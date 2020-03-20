82afd9522607fbadd17ffb2c8d44d3630981c461
charon吴 <wukaiismyh@gmail.com>
2020-03-20 11:17:19 +0800

    [init]

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="container">
+		<view class="">
+			你好，小程序
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/collocation/pages?id=easycom'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.intro {
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
