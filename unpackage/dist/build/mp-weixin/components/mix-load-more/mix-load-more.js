a74686dbc9905b3b84938c781d737957679c150f
charon吴 <wukaiismyh@gmail.com>
2020-05-13 18:37:27 +0800

    【=】

---
+++ unpackage/dist/build/mp-weixin/components/mix-load-more/mix-load-more.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/mix-load-more/mix-load-more"],{"46ee":function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0;var u={name:"mix-load-more",props:{status:{type:Number,default:0},text:{type:Array,default:function(){return["上拉显示更多","正在加载...","没有更多数据了"]}}}};e.default=u},"46fd":function(t,e,n){"use strict";n.r(e);var u=n("b6e8"),r=n("d013");for(var a in r)"default"!==a&&function(t){n.d(e,t,(function(){return r[t]}))}(a);n("6b89");var o,c=n("f0c5"),f=Object(c["a"])(r["default"],u["b"],u["c"],!1,null,null,null,!1,u["a"],o);e["default"]=f.exports},"6b89":function(t,e,n){"use strict";var u=n("9199"),r=n.n(u);r.a},9199:function(t,e,n){},b6e8:function(t,e,n){"use strict";var u,r=function(){var t=this,e=t.$createElement;t._self._c},a=[];n.d(e,"b",(function(){return r})),n.d(e,"c",(function(){return a})),n.d(e,"a",(function(){return u}))},d013:function(t,e,n){"use strict";n.r(e);var u=n("46ee"),r=n.n(u);for(var a in u)"default"!==a&&function(t){n.d(e,t,(function(){return u[t]}))}(a);e["default"]=r.a}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/mix-load-more/mix-load-more-create-component',
+    {
+        'components/mix-load-more/mix-load-more-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("46fd"))
+        })
+    },
+    [['components/mix-load-more/mix-load-more-create-component']]
+]);
